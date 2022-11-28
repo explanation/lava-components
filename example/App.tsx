8578dcb4f1968344591fab2edba7c5053775f17a
Nicholas Bonatsakis <nbonatsakis@gmail.com>
2022-11-28 15:34:32 -0500

    - Restructure project

---
+++ example/App.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import SearchBarScreen from '../lib/screens/SearchBarScreen'
+
+
+export default function App() {
+  const [path, setPath] = useState<string | null>(null)
+
+  return (
+    <View style={styles.container}>
+
+        {path == null &&
+        <View>
+            <Text>Pick a Screen{"\n\n"}</Text>
+            <Button
+                onPress={() => setPath('search')}
+                title="Search"
+              />
+        </View>}
+
+        {path == 'search' &&
+            <SearchBarScreen />
+        }
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
