960b2ed36cb37fa02729ee19c3ec9607e476feb2
Nicholas Bonatsakis <nbonatsakis@gmail.com>
2023-05-17 16:08:08 -0400

    - Fix merge

--- components/molecules/Friend/FriendGroup.tsx
+++ components/molecules/Friend/FriendGroup.tsx
@@ -31,57 +31,6 @@ export interface FriendProps {
     onPress?: () => void
 }
 
-<<<<<<< Updated upstream
-=======
-type FriendCircleProps = Pick<FriendGroupItem, 'imageUrl' | 'networkStatus'> & {
-    imageSize: number
-    containerSize: number
-    gap: number
-}
-
-export const FriendCircle: React.FC<FriendCircleProps> = (props) => {
-    const {imageUrl, networkStatus, imageSize, containerSize, gap} = props
-
-    const theme = useTheme()
-
-    const imageContainerBorderColor = () => {
-        return networkStatus === 'offline'
-            ? theme.colors.primarySand40
-            : theme.colors.tertiaryOnline
-    }
-
-    const styles = () => {
-        return StyleSheet.create({
-            imageContainer: {
-                height: containerSize + gap + 2,
-                width: containerSize + gap + 2,
-                borderRadius: containerSize,
-                justifyContent: 'center',
-                alignItems: 'center',
-                backgroundColor: theme.colors.secondaryContainers,
-                borderWidth: 2,
-                borderColor: imageContainerBorderColor(),
-            },
-            image: {
-                width: imageSize,
-                height: imageSize,
-                borderRadius: imageSize,
-            },
-        })
-    }
-
-    return (
-        <View style={styles().imageContainer}>
-            <Image
-                source={{uri: imageUrl}}
-                style={styles().image}
-                resizeMode="cover"
-            />
-        </View>
-    )
-}
-
->>>>>>> Stashed changes
 const FriendGroup: React.FC<FriendProps> = (props) => {
     const {
         friends = [],
